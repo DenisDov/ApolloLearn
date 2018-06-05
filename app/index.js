80938b1407d3cb61107963ebcaabf14a6383078a
Denis Dovzhenko <dovzdenis@gmail.com>
2018-06-05 12:13:46 +0300

    first commit

---
+++ app/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
