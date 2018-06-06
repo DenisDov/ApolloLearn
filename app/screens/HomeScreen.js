35bbc58c8e2e986866ef9e01c8c89e18b52554bb
Denis Dovzhenko <dovzdenis@gmail.com>
2018-06-06 10:27:45 +0300

    add touchable to post

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button, FlatList } from 'react-native';
+import { TouchableOpacity, Text, Button, FlatList } from 'react-native';
 import PropTypes from 'prop-types';
 
 import { Query } from 'react-apollo';
@@ -7,24 +7,38 @@ import gql from 'graphql-tag';
 
 import { Container } from './../components/Container';
 
+const GET_POSTS = gql`
+  {
+    allPosts {
+      id
+      title
+    }
+  }
+`;
+
 const PostsQuery = () => (
-  <Query
-    query={gql`
-      {
-        allPosts {
-          id
-          title
-        }
-      }
-    `}
-  >
+  <Query query={GET_POSTS}>
     {({ loading, error, data }) => {
       if (loading) return <Text>Loading...</Text>;
       if (error) return <Text>Error :(</Text>;
       return (
         <FlatList
           data={data.allPosts}
-          renderItem={({ item }) => <Text>{item.title}</Text>}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => console.log('press post id: ', item.id)}
+              style={{
+                padding: 16,
+                backgroundColor: 'white',
+                marginBottom: 10,
+                borderWidth: 0.5,
+                borderColor: '#ddd',
+                borderRadius: 5,
+              }}
+            >
+              <Text>{item.title}</Text>
+            </TouchableOpacity>
+          )}
           keyExtractor={item => item.id}
         />
       );
